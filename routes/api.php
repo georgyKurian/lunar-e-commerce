7b65a0ac190ed5a1669ac29cea516ac23fa4a640
Georgi Kurian <georgi@worksitesafety.ca>
2024-03-27 15:49:18 -0400

    ini

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    foreach (glob(__DIR__.'/api/v1/*.php') as $filename) {
+        include $filename;
+    }
+});
