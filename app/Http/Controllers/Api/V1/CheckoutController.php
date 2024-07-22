4243ee6168a6907b23df54f08dd5e9c1f520de84
Georgi Kurian <georgi@worksitesafety.ca>
2024-07-22 10:02:34 -0400

    Added payment intent code

---
+++ app/Http/Controllers/Api/V1/CheckoutController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Domain\Cart\Actions\AddToCartAction;
+use Domain\Cart\Actions\CreateCartAction;
+use Illuminate\Http\Request;
+use Lunar\Base\CartSessionInterface;
+use Lunar\Stripe\Facades\Stripe;
+
+class CheckoutController extends Controller
+{
+    public function __invoke(Request $request, CartSessionInterface $cartSession, CreateCartAction $createCartAction, AddToCartAction $addToCartAction)
+    {
+        $cart = $cartSession->current();
+
+        $intent = Stripe::createIntent($cart, opts:[]);
+
+        if ($intent->amount != $cart->total->value) {
+            Stripe::syncIntent($cart);
+        }
+
+        return $intent;
+    }
+}
